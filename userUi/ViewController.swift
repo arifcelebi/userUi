fcc55a39142411e451531925219ae1bbcdece914
arifcelebi <61031852+arifcelebi@users.noreply.github.com>
2022-08-24 17:38:01 +0300

    Initial Commit

---
+++ userUi/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  userUi
+//
+//  Created by Arif Celebi on 24.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.isHidden = true
+
+    }
+   
+
+}
+
